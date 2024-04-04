bcf8e9c68bf47c2bb119238ab715dc06065e2572
ClauTG <claus.trejo@gmail.com>
2024-04-04 10:30:24 +0200

    Chapter 4 Parameterized test case

--- tests/test_math.py
+++ tests/test_math.py
@@ -1,17 +1,14 @@
 import pytest
 
-def test_one_plus_one():
-    assert 1+1 == 2
+products = [
+    (2,3,6),
+    (99,1,99),
+    (0,1,0),
+    (3,-4,-12),
+    (-3,-3,9),
+    (2.5,6.7,16.75)
+]
 
-
-def test_one_plus_two():
-    a = 1
-    b = 2
-    c = 0
-    assert a+b == c
-
-def test_divide_by_zero():
-    with pytest.raises(ZeroDivisionError) as e:
-        num = 1/0
-    
-    assert 'division by zero' in str(e.value)
+@pytest.mark.parametrize('a,b,product', products)
+def test_multiplication(a,b, product):
+    assert a*b==product
