2d136d04621ecc62dd472ca2bdf2ada900cf206a
ClauTG <claus.trejo@gmail.com>
2024-04-17 11:45:27 +0200

    Testing popular pet store API

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,15 +2,31 @@ import pytest
 import requests
 pytestmark = pytest.mark.api
 
+
 @pytest.mark.duckduckgo
 def test_duckduckgo_instant_answer_api():
-        # Arrange
-        url = 'https://api.duckduckgo.com/?q=python+programming&format=json&pretty=1'
+    # Arrange
+    url = 'https://api.duckduckgo.com/?q=python+programming&format=json&pretty=1'
 
-        # Act
-        response = requests.get(url)
-        body =  response.json()
+    # Act
+    response = requests.get(url)
+    body = response.json()
 
-        # Assert
-        assert response.status_code == 200
-        assert 'Python' in body['AbstractText']
+    # Assert
+    assert response.status_code == 200
+    assert 'Python' in body['AbstractText']
+
+
+@pytest.mark.pet_store
+def test_available_pets_in_store():
+    # Arrange
+    url = "https://petstore3.swagger.io/api/v3/pet/findByStatus"
+    
+    # Act
+    response = requests.get(url, params={"status": "available"},)
+    body = response.json()
+
+    # Assert
+    assert response.status_code == 200
+    for pet in body:
+        assert pet['status'] == "available"
